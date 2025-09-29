5a333bfeb1e82ec2aef2de61cf30085bbe60b522
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-29 15:23:27 -0500

    feat: add new Get mapping on pizza controller for get a pizza by id parameter

--- src/main/java/llortegall/pizzeria/web/controller/PizzaController.java
+++ src/main/java/llortegall/pizzeria/web/controller/PizzaController.java
@@ -5,6 +5,7 @@ import llortegall.pizzeria.services.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class PizzaController {
 
     @GetMapping
     public ResponseEntity<List<PizzaEntity>> getAll(){
-        return ResponseEntity.ok(this.pizzaService.getAll());
+        return ResponseEntity.ok(this.pizzaService.getAllPizzas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PizzaEntity> getById(@PathVariable int id){
+        return ResponseEntity.ok(this.pizzaService.getPizzaById(id));
     }
 }
