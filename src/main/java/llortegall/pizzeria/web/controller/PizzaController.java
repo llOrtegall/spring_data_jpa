7ed3bc7d23c3847b6d3c7889ddc3ff3974f39971
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-29 14:58:48 -0500

    feat: created a new Pizza Controller and add two news GetMapping Spring data

---
+++ src/main/java/llortegall/pizzeria/web/controller/PizzaController.java
@@ -0,0 +1,27 @@
+package llortegall.pizzeria.web.controller;
+
+import llortegall.pizzeria.persistence.entity.PizzaEntity;
+import llortegall.pizzeria.services.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pizzas")
+public class PizzaController {
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaEntity>> getAll(){
+        return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+}
