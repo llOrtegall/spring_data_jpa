74067647e9e62d158978ce4456a8cdf7ced7d433
llOrtegall <IvanOrtega_97@hotmail.com>
2025-10-06 11:44:42 -0500

    feat: adds new querymethods on PizzaRepository

--- src/main/java/llortegall/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/llortegall/pizzeria/persistence/repository/PizzaRepository.java
@@ -3,6 +3,11 @@ package llortegall.pizzeria.persistence.repository;
 import llortegall.pizzeria.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+import java.util.List;
 
+public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String desc);
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String desc);
 }
