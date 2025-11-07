c6e8b34a60dc97b05036ce9380ec444b9d9b234e
llOrtegall <IvanOrtega_97@hotmail.com>
2025-11-07 12:02:43 -0500

    feat: add two new querymethods on OrderRepository

--- src/main/java/llortegall/pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/llortegall/pizzeria/persistence/repository/OrderRepository.java
@@ -3,5 +3,10 @@ package llortegall.pizzeria.persistence.repository;
 import llortegall.pizzeria.persistence.entity.OrderEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+    List<OrderEntity> findAllByDateAfter(LocalDateTime date);
+    List<OrderEntity> findAllByMethodIn(List<String> methods);
 }
