f3112ad9848336689e18a5455ea9179ebcfd0445
llOrtegall <IvanOrtega_97@hotmail.com>
2025-11-07 12:19:09 -0500

    refactor: add new repository to get 3 top price pizzas

--- src/main/java/llortegall/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/llortegall/pizzeria/persistence/repository/PizzaRepository.java
@@ -4,11 +4,13 @@ import llortegall.pizzeria.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
     List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
-    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+    Optional<PizzaEntity> findFirstByAvailableTrueAndNameIgnoreCase(String name);
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String desc);
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String desc);
+    List<PizzaEntity>findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
     int countAllByVeganTrue();
 }
