6926e191a9b5129cfe0cc662e1bab666c890d3fc
llOrtegall <IvanOrtega_97@hotmail.com>
2025-11-07 12:03:43 -0500

    feat: created a CONSTANTS for methods delibery and two new services

--- src/main/java/llortegall/pizzeria/services/OrderService.java
+++ src/main/java/llortegall/pizzeria/services/OrderService.java
@@ -5,11 +5,17 @@ import llortegall.pizzeria.persistence.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepo;
+    private static final String DELIVERY = "D";
+    private static final String CARRYOUT = "C";
+    private static final String ON_SITE = "S";
 
     @Autowired
     public OrderService(OrderRepository orderRepo) {
@@ -18,5 +24,15 @@ public class OrderService {
 
     public List<OrderEntity> getAllOrders(){
         return this.orderRepo.findAll();
+    }
+
+    public List<OrderEntity> getTodayOrders(){
+        LocalDateTime today = LocalDate.now().atTime(0, 0);
+        return this.orderRepo.findAllByDateAfter(today);
+    }
+
+    public List<OrderEntity> getOutSideOrders(){
+        List<String> methods = Arrays.asList(DELIVERY, CARRYOUT);
+        return this.orderRepo.findAllByMethodIn(methods);
     }
 }
