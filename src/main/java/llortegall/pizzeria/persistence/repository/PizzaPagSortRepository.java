042a607730522d688ebaf036e5653ea59824e2bb
llOrtegall <IvanOrtega_97@hotmail.com>
2025-11-07 12:38:18 -0500

    feat: created a new repository and using pagination and sort interface for pizzas repo

---
+++ src/main/java/llortegall/pizzeria/persistence/repository/PizzaPagSortRepository.java
@@ -0,0 +1,8 @@
+package llortegall.pizzeria.persistence.repository;
+
+import llortegall.pizzeria.persistence.entity.PizzaEntity;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+
+public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+
+}
