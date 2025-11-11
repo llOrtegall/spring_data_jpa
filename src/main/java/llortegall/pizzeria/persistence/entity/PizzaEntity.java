7233b42085939a20fc4f1362937fe0cba3684cd5
llOrtegall <IvanOrtega_97@hotmail.com>
2025-11-11 11:52:36 -0500

    refactor: editor use * for all imports

--- src/main/java/llortegall/pizzeria/persistence/entity/PizzaEntity.java
+++ src/main/java/llortegall/pizzeria/persistence/entity/PizzaEntity.java
@@ -1,11 +1,6 @@
 package llortegall.pizzeria.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
